43981483a9500a9c632977a6b9564820d44b824a
grbalmeida <g.r.almeida@live.com>
2023-08-29 13:08:00 -0300

    Trabalhando com textos

--- primeiroprojeto/src/Main.java
+++ primeiroprojeto/src/Main.java
@@ -8,5 +8,14 @@ public class Main {
 
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+
+        // Média calculada pelas 3 notas da Jack, Paulo e Suelem
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = "Filme de aventura com galã dos anos 80";
+
+        System.out.println(sinopse);
     }
 }
