888e758eec542d89479d285730ecfbd8b601befe
grbalmeida <g.r.almeida@live.com>
2023-08-30 13:56:40 -0300

    Montando o menu de opções

--- desafio/src/Desafio.java
+++ desafio/src/Desafio.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class Desafio {
     public static void main(String[] args) {
         String nome = "Clark Kent";
         String tipoConta = "Corrente";
         double saldo = 1599.99;
+        int opcao = 0;
 
         System.out.println("*****************************");
         System.out.println("\nNome do cliente: " + nome);
         System.out.println("Tipo conta: " + tipoConta);
         System.out.println("Saldo atual: " + saldo);
         System.out.println("\n*****************************");
+
+        StringBuilder menu = new StringBuilder();
+        menu.append("\n** Digite sua opção **");
+        menu.append("\n1 - Consultar saldo");
+        menu.append("\n2 - Transferir valor");
+        menu.append("\n3 - Receber valor");
+        menu.append("\n4 - Sair");
+
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+        }
     }
 }
